31900edb4c6e3d67fd2e1975f8dd90d40802c380
Guy Steinberg <guysteinberg123@gmail.com>
2019-12-01 07:29:41 +0200

    Finished Lesson 4: exercise 2
    Added ability to pass data between intents using Intent extras,
    and parsing data using getExtras and bundle

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,31 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    TextView mDisplayWeatherTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        mDisplayWeatherTV = findViewById(R.id.weather_data_detail_tv);
         // TODO (2) Display the weather forecast that was passed from MainActivity
+        Intent parentIntent = getIntent();
+
+        if(parentIntent.hasExtra("Weather")) {
+            Bundle extras = parentIntent.getExtras();
+            String weatherData = extras.getString("Weather");
+            mDisplayWeatherTV.setText(weatherData);
+        }
+
+
     }
 }
